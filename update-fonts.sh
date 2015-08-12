508d90b18e756e2ee2216c7785dd056dc565d927
Daniel Freedman <dfreedm@google.com>
2015-08-12 11:53:42 -0700

    Rebase from github.com/google/fonts
    Fixes #2

--- update-fonts.sh
+++ update-fonts.sh
@@ -8,23 +8,40 @@
 # subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 set -x
 
-if [ ! -d .roboto ]; then
-  git clone --no-checkout https://github.com/google/roboto.git .roboto
-  pushd .roboto
+if [ ! -d .fonts ]; then
+  git clone --no-checkout https://github.com/google/fonts.git .fonts
+  pushd .fonts
   git config core.sparsecheckout true
-  echo "out/RobotoTTF" >> .git/info/sparse-checkout
-  echo "LICENSE" >> .git/info/sparse-checkout
+  echo "apache/roboto" >> .git/info/sparse-checkout
+  echo "apache/robotomono" >> .git/info/sparse-checkout
   git checkout
   popd
 else
-  pushd .roboto
+  pushd .fonts
   git pull
   popd
 fi
 
-rm -rf RobotoTTF
-mkdir RobotoTTF
-cp -r .roboto/out/RobotoTTF/* RobotoTTF
-cp .roboto/LICENSE RobotoTTF
+rm -rf fonts
+mkdir fonts
+cp -r .fonts/apache/* fonts
+
+cat > roboto.html <<END
+<!--
+@license
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+-->
+<style>
+END
+
+node generate-style.js >> roboto.html
+
+echo "</style>" >> roboto.html
+
 
 echo "Fonts Updated"
