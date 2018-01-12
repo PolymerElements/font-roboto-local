e04ef0d916e49193fd80c66d0e26c63ec239cf52
Fred K. Schott <fschott@google.com>
2018-01-11 19:06:48 -0800

    auto-converted by polymer-modulizer

--- roboto.js
+++ roboto.js
@@ -1,13 +1,7 @@
-<!--
-@license
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-<style>
+const $_documentContainer = document.createElement('div');
+$_documentContainer.setAttribute('style', 'display: none;');
+
+$_documentContainer.innerHTML = `<style>
 @font-face {
 font-family: "Roboto";
 src: url(fonts/roboto/Roboto-Thin.ttf) format("truetype");
@@ -140,4 +134,17 @@ src: url(fonts/robotomono/RobotoMono-BoldItalic.ttf) format("truetype");
 font-weight: 700;
 font-style: italic;
 }
-</style>
+</style>`;
+
+document.head.appendChild($_documentContainer);
+
+/**
+@license
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+;
